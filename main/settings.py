46e81c81ec75785a12cd8ab8a03b9d7e97990895
Aditya Vishwakarma <vishwakarma9408@gmail.com>
2025-07-20 02:16:48 +0530

    Update deploy

--- main/settings.py
+++ main/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # your static folder inside project
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
